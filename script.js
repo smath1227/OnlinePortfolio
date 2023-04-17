578e3ef5443420cd6b31522b12b905aecb96328d
smath1227 <6423836-smath1227@users.noreply.replit.com>
2023-04-17 04:07:13 +0000

    2023 portfolio

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
-$(document).ready(function() {
-    var target = $(location).attr("hash");
-    var offset = ($(this).attr('data-offset') ? $(this).attr('data-offset') : 0);
-    $('body,html').animate({
-        scrollTop: $(target).offset().top - offset
-    }, 1000);
-});
+var i = 0;
+var txt = 'Lorem ipsum typing effect!'; /* The text */
+var speed = 50; /* The speed/duration of the effect in milliseconds */
+
+function typeWriter() {
+  if (i < txt.length) {
+    document.getElementById("demo").innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  }
+}
